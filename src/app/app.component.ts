0f735cc0553aeae254f84b2bb41c62392dcd8823
BabuRam123 <rbabutech8@gmail.com>
2019-10-03 12:55:48 +0530

    4 files modified, 4 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AccountInformation} from './AccountInformation';
 
 @Component({
   selector: 'my-app',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Display Account details using ngIF and ngFor';
+  name = 'Display Account details using oneway binding, ngIF, ngFor';
+  twowaytext='Twoway binding';
+  setTwowaybind:string="Hello";
+  accountlist=[
+    new AccountInformation(1,'Siva'),
+    new AccountInformation(2,'Babu'),
+    new AccountInformation(3,'Arun'),
+    new AccountInformation(4,'Nandha')
+  ];
+
+  
+
 }
